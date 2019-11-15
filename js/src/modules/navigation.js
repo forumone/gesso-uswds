015aa79e3d603b7229598aa25b7076f246bf2cb4
Kelli Monahan <kmonahan@forumone.com>
2019-11-15 17:31:58 -0600

    Extend USWDS menu components

--- js/src/modules/navigation.js
+++ js/src/modules/navigation.js
@@ -1,10 +1,10 @@
 import navigation from 'uswds/src/js/components/navigation.js';
 
 export default function() {
-  const subnav = document.querySelectorAll('.usa-nav__submenu');
+  const subnav = document.querySelectorAll('.menu__subnav');
   subnav.forEach((menu, index) => {
     const button = menu.previousElementSibling;
-    if (button.classList.contains('usa-accordion__button')) {
+    if (button.classList.contains('usa-nav__link')) {
       const id = `extended-nav-section-${index}`;
       menu.id = id;
       button.setAttribute('aria-controls', id);
