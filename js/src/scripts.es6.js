319000785f30c43d450e2aea35fcdd10424b7898
KJ Monahan <kmonahan@forumone.com>
2020-05-20 10:14:55 -0500

    Add back to top (#333)
    * Generate single CSS file for pattern lab
    
    * Create Back to Top component
    
    * Include back to top on page
    
    * Remove old code
    
    * Remove currentColor from SVG + add body id
    
    * CR fixes for back to top
    
    * Add style.css back
    
    Co-authored-by: Corey Lafferty <clafferty@forumone.com>

--- js/src/scripts.es6.js
+++ js/src/scripts.es6.js
@@ -7,6 +7,7 @@ import domready from 'domready';
 import accordion from 'uswds/src/js/components/accordion.js';
 import navigation from './modules/navigation';
 import banner from 'uswds/src/js/components/banner.js';
+import backToTop from './modules/_back-to-top.es6';
 
 (function() {
   'use strict';
@@ -28,9 +29,11 @@ import banner from 'uswds/src/js/components/banner.js';
     timeOut = setTimeout(resizeStuff, 200);
   });
 
+  // Any scripts you want to initialize once the DOM is ready go here.
   domready(() => {
     accordion.on(document.body);
     banner.on(document.body);
     navigation(); // If used with the USWDS accordion component, the navigation must run after it.
+    backToTop();
   });
 })();
