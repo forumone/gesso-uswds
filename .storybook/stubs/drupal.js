2d979c279e79b3690c46a2d29a37a9d5e231a7d0
Corey Lafferty <coreylafferty@gmail.com>
2025-04-01 11:16:29 -0400

    Add External Link component

--- .storybook/stubs/drupal.js
+++ .storybook/stubs/drupal.js
@@ -34,7 +34,16 @@ const Drupal = { behaviors: {} };
     return Drupal.theme[themeFunction](options);
   };
 
-  drupalSettings.guswds = {
+  drupalSettings.gesso = {
+    externalLinkExitDisclaimer: 'Exit this website',
+    externalLinkAllowedDomains: [
+      'example-allowed-domain.com',
+      'forumone.github.io',
+    ],
+    externalLinkAllowedLinks: [
+      'https:\/\/www.vimeo.com\/example-allowed-link',
+      'https:\/\/www.youtube.com\/example-allowed-link',
+    ],
     imagePath: 'images',
   };
 })(Drupal, window.drupalSettings);
