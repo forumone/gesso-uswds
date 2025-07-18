bf1a4c98bd2cb734593807a1be35902bb8a05036
Corey Lafferty <coreylafferty@gmail.com>
2025-07-18 13:41:55 -0400

    refactor srollbar width

--- source/01-global/html-elements/00-universal/universal.es6.js
+++
@@ -1,8 +0,0 @@
-import Drupal from 'drupal';
-import setScrollbarProperty from '../../../07-utility/_setScrollbarProperty.es6';
-
-Drupal.behaviors.setScrollbarProperty = {
-  attach() {
-    setScrollbarProperty();
-  },
-};
